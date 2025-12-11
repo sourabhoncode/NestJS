6168c674a2bfd57e2a50b5209d3eee768e61e059
Sourabh Kumar <sourabhshris12@gmail.com>
2025-12-11 15:03:12 +0530

    Refactor: Move parking module inside vehicle, remove Prettier, update imports

--- src/vehicle/dto/create-vehicle.dto.ts
+++ src/vehicle/dto/create-vehicle.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateVehicleDto {
+    @IsNotEmpty()
+    @IsString()
     vehicleNumber: string;
+
+    @IsNotEmpty()
+    @IsString()
     model: string;
+
+    @IsNotEmpty()
+    @IsString()
     brand: string;
 }
